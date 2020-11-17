2b3d1b869e7fcd7095a189b46c8160533e7d5ad5
Peter Skarpetis <peters@skarpetis.com>
2020-11-17 14:19:16 +1100

    set logger debuglevel to info enabled 150ms Sleep hack to fix error with setting direction

--- logger.go
+++ logger.go
@@ -5,6 +5,6 @@ import logger "github.com/d2r2/go-logger"
 // You can manage verbosity of log output
 // in the package by changing last parameter value.
 var lg = logger.NewPackageLogger("dht",
-	logger.DebugLevel,
-	// logger.InfoLevel,
+	//logger.DebugLevel,
+	logger.InfoLevel,
 )
